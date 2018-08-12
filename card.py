e86a6fd2cb39080e18960f8259a6f3fc8a6e85a1
3tilley <3tilley@googlemail.com>
2018-08-12 22:37:49 +0100

    With code for cards

---
+++ card.py
@@ -0,0 +1,13 @@
+class Card(object):
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+
+def make_card(card_str):
+    suit = card_str[-1].upper()
+    assert suit in ["SHDC"], "Invalid suit"
+    rank = card_str[:-1].upper()
+    assert rank in ["123456789JQKA"] or rank == "10", "Invalid rank"
+    return Card(suit, rank)
+
+cards = [make_card(c) for c in ["AS", "10H", "QD", "QC", "KC", "2D", "3D", "4D", "5D", "6D", "2C", "3C", "4C"]
