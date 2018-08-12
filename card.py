cfdc03f5e07259ee0ee52c5cd9426581ee9b1f21
3tilley <3tilley@googlemail.com>
2018-08-12 22:52:28 +0100

    Fix card bug

--- card.py
+++ card.py
@@ -5,9 +5,9 @@ class Card(object):
 
 def make_card(card_str):
     suit = card_str[-1].upper()
-    assert suit in ["SHDC"], "Invalid suit"
+    assert suit in "SHDC", "Invalid suit " + suit
     rank = card_str[:-1].upper()
-    assert rank in ["123456789JQKA"] or rank == "10", "Invalid rank"
+    assert rank in "123456789JQKA" or rank == "10", "Invalid rank " + rank
     return Card(suit, rank)
 
-cards = [make_card(c) for c in ["AS", "10H", "QD", "QC", "KC", "2D", "3D", "4D", "5D", "6D", "2C", "3C", "4C"]
+cards = [make_card(c) for c in ["AS", "10H", "QD", "QC", "KC", "2D", "3D", "4D", "5D", "6D", "2C", "3C", "4C"]]
