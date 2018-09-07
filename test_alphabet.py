6533bf7268710c436c1773a4f8e563a923bee005
3tilley <3tilley@googlemail.com>
2018-09-07 22:22:26 +0100

    With alphabet fix

--- test_alphabet.py
+++ test_alphabet.py
@@ -2,7 +2,7 @@ from alphabet import count_vowels, charlotte
 
 alphabet = map(chr, range(65, 91))
 
-def test_count_vowels():
+def test_count_all_vowels():
     assert count_vowels(alphabet) == 5
 
 def test_count_vowels():
