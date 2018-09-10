d8d3a71da2b0440e4303ae36555f08bc04274ba7
3tilley <3tilley@googlemail.com>
2018-09-10 22:21:49 +0100

    basic hand test

--- test_hand.py
+++ test_hand.py
@@ -1,5 +1,13 @@
 import start
 import card
+import hand
+
+
+balanced_cards = ["AS", "KS", "QS", "AH", "KH", "QH",
+    "AD", "KD", "QD", "JD", "AC", "KC", "QC"]
+
+unbalanced_cards = ["AS", "KS", "QS", "JS", "10S", "9S",
+    "8S", "7S", "6S", "5S", "4S", "3S", "2S"]
 
 def test_calculate_points():
 
@@ -9,18 +17,18 @@ def test_calculate_points():
     assert pts == 11
 
 def test_is_balanced_hand():
-    balanced_cards = ["AS", "KS", "QS", "AH", "KH", "QH",
-    "AD", "KD", "QD", "JD", "AC", "KC", "QC"]
     hand = [card.make_card(c) for c in balanced_cards]
     is_balanced = start.is_balanced_hand(hand)
     assert is_balanced == True
 
 def test_is_unbalanced_hand():
-    unbalanced_cards = ["AS", "KS", "QS", "JS", "10S", "9S",
-    "8S", "7S", "6S", "5S", "4S", "3S", "2S"]
     
     hand = [card.make_card(c) for c in unbalanced_cards]
     
     is_balanced = start.is_balanced_hand(hand)
     
     assert is_balanced == False
+
+def test_hand_pretty_print():
+    hand = hand.Hand(balanced_cards)
+    assert hand.pretty_print() == ""
