886b61e89e6003db03aae2c73c31d2d08ebac403
3tilley <3tilley@googlemail.com>
2018-09-10 21:49:30 +0100

    Hand class with methods

--- hand.py
+++ hand.py
@@ -1,3 +1,19 @@
-class Hand(object):
+class Hand():
     def __init__(self, cards):
         self.cards = cards
+
+    def cards_by_suit(self):
+        cards_dict = {
+            "S": [],
+            "H": [],
+            "D": [],
+            "C": []
+        }
+        for c in self.cards:
+            cards_dict[c.suit].append(c)
+        
+        return cards_dict
+
+    def pretty_print(self):
+        
+        return "a string"
